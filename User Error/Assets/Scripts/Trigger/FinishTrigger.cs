c3ec8316d105c164c2ac31633af521fcd75eca36
scripaaa <roadtosad@gmail.com>
2025-12-05 19:05:37 +0300

    merge fixes

--- User Error/Assets/Scripts/Trigger/FinishTrigger.cs
+++ User Error/Assets/Scripts/Trigger/FinishTrigger.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Finish : MonoBehaviour
 {
     public GameObject finishCanvas;
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
