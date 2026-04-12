f0a76d29edd65932d0aa658d09b93b1137818ece
scripaaa <roadtosad@gmail.com>
2026-04-12 13:09:00 +0300

    Displaying the icon of the item being collected

---
+++ User Error/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+    [SerializeField] private List<Image> cellIcons;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddItemToCell(ItemData itemToAdd)
+    {
+        if (itemToAdd == null) return;
+
+        for (int i = 0; i < cellIcons.Count; i++)
+        {
+            // Проверяем именно по активности объекта — надёжно и просто
+            if (!cellIcons[i].gameObject.activeSelf)
+            {
+                cellIcons[i].sprite = itemToAdd.itemIcon;
+                cellIcons[i].color = Color.white;
+                cellIcons[i].gameObject.SetActive(true);
+
+                Debug.Log($"<color=cyan>Инвентарь:</color> добавлен {itemToAdd.itemName} в слот {i}");
+                return;
+            }
+        }
+
+        Debug.LogWarning("Инвентарь полон!");
+    }
+}
